f91bdcbd499e7394c03cc3d87e4623fc83eb94f2
Your Name <you@example.com>
2026-04-14 15:08:05 -0500

    fourth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./features/layout/components/Header";
 import { Content } from "./features/layout/components/Content";
 import { Footer } from "./features/layout/components/Footer";
-import  Login  from "./features/auth/Components/login";
-import  Register  from "./features/auth/Components/register";
+import Login from "./features/auth/Components/Login";
+import Register from "./features/auth/Components/Register";
 import RickAndMorty from "./features/api/rickandmorty";
 import Dashboard from "./features/dashboard/Dashboard";
 import { HashRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <HashRouter>
@@ -14,10 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Content />} />
         <Route path="/rickandmorty" element={<RickAndMorty />} />
-        <Route path="/Api" element={<RickAndMorty />} />
+        <Route path="/api" element={<RickAndMorty />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
 
       <Footer />
